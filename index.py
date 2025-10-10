9ca779c500ab28f2f9b970823cbbb3256771ea5b
Karan Prajapati <karanpraja6@gmail.com>
2025-10-10 15:40:52 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,22 @@
+# print("Hello World")
+# a=int(input("Enter a number: ")) 
+# b=int(input("Enter another number: "))
+# print(a+b)
+# print(a-b)
+
+# a=int(input("A= "))
+# b=int(input("B= "))
+# c=a//b #most correct value
+# c = a**b
+
+
+# exchange the value of a and b wothout using third variable, use bitwise operator only.
+a = 5
+b = 6
+a = a ^ b
+b = a ^ b
+a = a ^ b
+print(a,b)
+
+
+# print(bin(c))
